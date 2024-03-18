87be40556d7d27690a392f47ccd0c48ff5cf986e
Iliain <drcv251@gmail.com>
2024-03-18 14:13:06 +1100

    Add Driver

---
+++ Silverstripe3ValetDriver.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Valet\Drivers\Custom;
+
+use Valet\Drivers\LaravelValetDriver;
+
+class Silverstripe3ValetDriver extends LaravelValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves(string $sitePath, string $siteName, string $uri): bool
+    {
+        if (file_exists($sitePath.'/cms') && file_exists($sitePath.'/framework')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile(string $sitePath, string $siteName, string $uri)/*: string|false */
+    {
+        if (file_exists($sitePath.$uri) &&
+            ! is_dir($sitePath.$uri) &&
+            pathinfo($sitePath.$uri)['extension'] != 'php') {
+            return $sitePath.$uri;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath(string $sitePath, string $siteName, string $uri): string
+    {
+        if (file_exists($sitePath.$uri) && ! is_dir($sitePath.$uri)) {
+            return $sitePath.$uri;
+        }
+
+        $_SERVER['PHP_SELF'] = '/framework/main.php';
+        $_SERVER['SCRIPT_NAME'] = '/framework/main.php';
+        $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/framework/main.php';
+        $_SERVER['QUERY_STRING'] = 'url='.$uri;
+        $_SERVER['DOCUMENT_URI'] = $uri;
+        $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+        $_GET['url'] = $uri;
+
+        return $sitePath.'/framework/main.php';
+    }
+}
